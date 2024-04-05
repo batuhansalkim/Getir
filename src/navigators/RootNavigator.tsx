1b2fe63c0a6bcf6902969991c941672050ba1f4b
batuhan <batusalkim11@gmail.com>
2024-04-05 04:35:32 +0300

    bottomtab navigator devam

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Entypo} from "@expo/vector-icons";
+import HomeNavigator from "./HomeNavigator";
+
+const Stack = createBottomTabNavigator();
 
 const RootNavigator = () => {
   return (
-    <View>
-      <Text>RootNavigator</Text>
-    </View>
+    <Stack.Navigator 
+    initialRouteName='Anasayfa' 
+    screenOptions={{
+        // tabBarHideOnKeyboard=true,
+        tabBarShowLabel:false,
+        tabBarActiveTintColor:"#5C3EBC",
+        tabBarInactiveTintColor:"#959595",
+        headerShown:false,
+        tabBarStyle:{
+            height:80,
+        }
+    }}>
+        <Stack.Screen 
+            name='Anasayfa' 
+            component={HomeNavigator} 
+            options={{
+                tabBarIcon:({color})=>(<Entypo name="home" size={24} color={color}/>)
+        }}/>
+    </Stack.Navigator>
   )
 }
 
