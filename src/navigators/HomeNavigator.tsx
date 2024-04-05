8035b049df58561f15ac484e93b63fe01a1dbc93
batuhan <batusalkim11@gmail.com>
2024-04-05 20:25:29 +0300

    update

--- src/navigators/HomeNavigator.tsx
+++ src/navigators/HomeNavigator.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Image, View } from "react-native";
 import {createStackNavigator} from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
+import CategoryFilterScreen from "../screens/CategoryFilterScreen";
+ 
 
 const Stack = createStackNavigator();
 
@@ -10,6 +13,30 @@ export default function index(){
             <Stack.Screen
                 name="Home"
                 component={HomeScreen}
+                options={{
+                    headerStyle:{backgroundColor:"#5C3EBC"},
+                    headerTitle:()=>(
+                            <Image source={require("../../assets/getirlogo.png")}
+                            style={{width:70,height:30,marginLeft:123
+                            }}
+                        />
+                    )
+                }}
+            />
+
+            <Stack.Screen
+                name="CategoryDetails"
+                component={CategoryFilterScreen}
+                options={{
+                    headerStyle:{backgroundColor:"#5C3EBC"},
+                    headerTitle:()=>(
+                            <Image source={require("../../assets/getirlogo.png")}
+                            style={{width:70,height:30,marginLeft:123
+                            }}
+                        />
+                        
+                    )
+                }}
             />
         </Stack.Navigator>        
     )
--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity } from 'react-native'
 import React from 'react'
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {Entypo} from "@expo/vector-icons";
+import {Entypo,FontAwesome,MaterialCommunityIcons} from "@expo/vector-icons";
 import HomeNavigator from "./HomeNavigator";
 
 const Stack = createBottomTabNavigator();
 
 const RootNavigator = () => {
+
+    const CustomTabBarButton = ({children})=>{
+        return(
+            <TouchableOpacity style={{
+                width:56,
+                height:56,
+                backgroundColor:"#5C3EBC",
+                justifyContent:"center",
+                alignItems:"center",
+                borderRadius:33,
+                marginTop:-8,
+                borderWidth:3,
+                borderColor:"white"
+            }}>
+                <Entypo name='list' size={32} color="#FFD00C"/>
+            </TouchableOpacity>
+        )
+    }
+
   return (
     <Stack.Navigator 
     initialRouteName='Anasayfa' 
@@ -17,7 +36,7 @@ const RootNavigator = () => {
         tabBarInactiveTintColor:"#959595",
         headerShown:false,
         tabBarStyle:{
-            height:80,
+            height:50,
         }
     }}>
         <Stack.Screen 
@@ -25,6 +44,33 @@ const RootNavigator = () => {
             component={HomeNavigator} 
             options={{
                 tabBarIcon:({color})=>(<Entypo name="home" size={24} color={color}/>)
+        }}/>
+
+        <Stack.Screen 
+            name='Search' 
+            component={HomeNavigator} 
+            options={{
+                tabBarIcon:({color})=>(<FontAwesome name="search" size={24} color={color}/>)
+        }}/>
+        <Stack.Screen
+            name='list'
+            component={HomeNavigator}
+            options={{
+                tabBarIcon:(props)=><CustomTabBarButton {...props}/>
+            }}
+        />
+        <Stack.Screen 
+            name='User' 
+            component={HomeNavigator} 
+            options={{
+                tabBarIcon:({color})=>(<FontAwesome name="user" size={24} color={color}/>)
+        }}/>
+
+        <Stack.Screen 
+            name='Gift' 
+            component={HomeNavigator} 
+            options={{
+                tabBarIcon:({color})=>(<MaterialCommunityIcons name="gift" size={24} color={color}/>)
         }}/>
     </Stack.Navigator>
   )
